35a195f32385f6eb1657a29895ca48a6663c988b
OldMan5119 <sx_mail@163.com>
2025-08-16 23:07:20 +0800

    提交

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://localhost:3443",
+    baseURL: "http://localhost:3000",
     withCredentials: true, // send cookies when cross-domain requests
     timeout: 6 * 1000 // request timeoutn
 })
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/rabbit/login': {
-                target: 'https://10.30.108.250:3443',
+                target: 'https://192.168.3.3:3000',
                 changeOrigin: true,
                 // pathRewrite: {'^/rabbit': ''},
                 secure: false, // 忽略 HTTPS 证书错误（仅开发环境）
